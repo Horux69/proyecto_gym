437d44b6356fb0d3d1b30f96bfc7557f16952f08
Camilo Castillo <horuxjcc@gmail.com>
2023-11-18 18:02:44 -0500

    crud membresias

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import hashlib
 from claseValidaLogin import ValidationLogin
 from claseOperadores import Operadores
+from claseMembresias import Membresias
 
 app = Flask(__name__)
 
@@ -33,6 +34,7 @@ cursor = conexion.cursor()
 
 validaLogin = ValidationLogin(mysql)
 losOperadores = Operadores(mysql)
+lasMembresias = Membresias(mysql)
 
 
 
@@ -142,6 +144,59 @@ def inventario():
 @app.route('/inventario/categorias')
 def categorias():
     return render_template('/dashboard/categoria_productos.html')
+
+# ------------------------ MEMBRESIAS ---------------------------------------------#
+
+@app.route('/membresias')
+def membresias():
+    if session.get("logueado"):
+        resultado = lasMembresias.consultarMembresias()
+        return render_template('dashboard/membresias.html', membresias = resultado)
+    else:
+        return redirect('/')
+    
+@app.route('/membresias/agregarMembresia', methods = ['POST'])
+def agregarMembresia():
+    if session.get("logueado") and session.get("rol") == 'administrador' or session.get("rol") == 'super_admin':
+        nombre = request.form['nombre']
+        duracion = request.form['duracion']
+        precio = request.form['precio']
+        estado = 'activo'
+
+        lasMembresias.agregarMembresia([nombre, duracion, precio, estado])
+
+        return redirect('/membresias')
+    else:
+        return redirect('/')
+    
+@app.route('/membresias/desactivarMembresia/<id_membresia>')
+def desactivarMembresia(id_membresia):
+    if session.get("logueado") and session.get("rol") == 'administrador' or session.get("rol") == 'super_admin':
+        lasMembresias.desactivarMembresia(id_membresia)
+        return redirect('/membresias')
+    else:
+        return redirect('/')
+    
+@app.route('/membresias/infoEdit/<id_membresia>', methods = ['GET'])
+def infoMembresiaEdit(id_membresia):
+    if session.get("logueado") and session.get("rol") == 'administrador' or session.get("rol") == 'super_admin':
+        resultado = lasMembresias.infoMembresia(id_membresia)
+
+        return render_template('dashboard/edit_membresia.html', editMembresia = resultado[0])
+    else:
+        return redirect('/')
+    
+@app.route('/membresias/editarMembresia', methods = ['POST'])
+def editMembresia():
+    _id = request.form['txtID']
+    nombre = request.form['nombre']
+    duracion = request.form['duracion']
+    precio = request.form['precio']
+
+    lasMembresias.editMembresia([_id, nombre, duracion, precio])
+
+    return redirect('/membresias')
+
 
 
 
