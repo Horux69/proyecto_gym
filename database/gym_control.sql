60e8bf4473e38d68ea3f3175eebecb992fdd96a2
Cristian Vanegas <pequeflow-2003@hotmail.com>
2024-02-27 10:33:11 -0500

    base de datos

--- database/gym_control.sql
+++ database/gym_control.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-02-2024 a las 05:34:28
+-- Tiempo de generación: 27-02-2024 a las 16:32:51
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -123,7 +123,8 @@ INSERT INTO `membresias` (`id_membresia`, `nombre`, `tiempo_duracion`, `precio`,
 (2, 'Semanal', 6, 15000, 'activo'),
 (3, 'Quincenal', 15, 30000, 'activo'),
 (4, 'Mensual', 30, 60000, 'activo'),
-(5, 'personalizada', 30, 300000, 'activo');
+(5, 'personalizada', 30, 300000, 'activo'),
+(6, 'Vencida', 0, 0, 'inactivo');
 
 -- --------------------------------------------------------
 
@@ -188,10 +189,11 @@ CREATE TABLE `registro_usuarios` (
 --
 
 INSERT INTO `registro_usuarios` (`cedula`, `nombre`, `apellido`, `fecha_nac`, `telefono`, `sexo`, `tipo_sangre`, `huella`, `nuemero_emergencia`, `correo`, `contrasena`, `tarjeta_nfc`, `id_membresia`, `fecha_inicio`, `fecha_vencimiento`, `fecha_registro`, `user_registro`, `estado`) VALUES
-('0202020202', 'Carlos Alberto', 'Posada', '2008-02-14', '3152085189', 'hombre', 'AB', 'NULL', 2147483647, 'lokobrs143@gmail.com', '0202020202', '10', 5, '2024-02-24', '2024-03-25', '2024-02-24', 'cristian', 'activo'),
-('1007412611', 'Camilo', 'Castillo', '2001-11-24', '3172509264', '', '', '', 0, 'horuxjcc@gmail.com', '', '126196196962', 4, '2023-11-19', '2023-12-19', '2023-11-19', 'Camilo', 'inactivo'),
-('1193592038', 'cristian', 'vanegas', '2003-10-28', '3152085189', '', '', '', 0, 'pequeflow-2003@hotmail.com', '', '01', 4, '2023-12-03', '2024-05-31', '2023-12-03', 'cristian', 'activo'),
-('16161', 'camilo', 'castillo', '2001-11-24', '56944194', '', '', '', 0, 'camilo@gmail.com', '', '255626', 2, '2023-10-11', '2023-11-11', '2023-10-11', 'camilo', 'inactivo');
+('0202020202', 'Carlos Alberto', 'Posada', '2008-02-14', '3152085189', 'hombre', 'AB', 'NULL', 2147483647, 'lokobrs143@gmail.com', '0202020202', '10', 4, '2024-02-24', '2024-04-24', '2024-02-24', 'cristian', 'activo'),
+('1007412611', 'Camilo', 'Castillo', '2001-11-24', '3172509264', '', '', '', 0, 'horuxjcc@gmail.com', '', '126196196962', 3, '0000-00-00', '2024-03-12', '2023-11-19', 'Camilo', 'inactivo'),
+('1193592038', 'cristian', 'vanegas', '2003-10-28', '3152085189', '', '', '', 0, 'pequeflow-2003@hotmail.com', '', '01', 5, '0000-00-00', '2024-03-27', '2023-12-03', 'cristian', 'inactivo'),
+('16161', 'camilo', 'castillo', '2001-11-24', '56944194', '', '', '', 0, 'camilo@gmail.com', '', '255626', 6, '2023-10-11', '2023-11-11', '2023-10-11', 'camilo', 'inactivo'),
+('232312222', 'alfonzo', 'alquaeda', '2008-02-21', '1222222222', 'hombre', 'AB', 'NULL', 2147483647, '12222222@nxjskdacns.com', '232312222', '25', 6, '2024-02-25', '2024-02-27', '2024-02-25', 'cristian', 'inactivo');
 
 --
 -- Índices para tablas volcadas
@@ -262,7 +264,7 @@ ALTER TABLE `medidas`
 -- AUTO_INCREMENT de la tabla `membresias`
 --
 ALTER TABLE `membresias`
-  MODIFY `id_membresia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_membresia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
